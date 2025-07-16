a27d286622780f5ea3a588a06fba51f2689ebfdf
nbetancourt <206279116+betancourtnic@users.noreply.github.com>
2025-07-16 18:32:49 -0500

    changed deprecated configurations for templates according to AWS updated practices

--- terraform-up-and-running-code-examples/ec2-instance-example/webserver-cluster/main.tf
+++ terraform-up-and-running-code-examples/ec2-instance-example/webserver-cluster/main.tf
@@ -4,17 +4,27 @@ provider "aws" {
 }
 
 # ASG configs
-resource "aws_launch_configuration" "example" {
+resource "aws_launch_template" "example" {
+    name_prefix = "webserver-template-"
     image_id    = "ami-020cba7c55df1f615"
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.instance.id]
 
-    user_data = <<-EOF
+    network_interfaces {
+        associate_public_ip_address = true
+        security_groups = [aws_security_group.instance.id]
+    }
+    
+
+    user_data = base64encode(<<-EOF
             #!/bin/bash
             echo "Hello, World" > index.html
             nohup busybox httpd -f -p ${var.server_port} &
             EOF
+    )
 
+    tags = {
+        Name = "webserver_launch_template"
+    }
     #this is required for using a launch config with an AGS
     lifecycle {
         create_before_destroy = true
@@ -23,7 +33,11 @@ resource "aws_launch_configuration" "example" {
 
 #defines ASG
 resource "aws_autoscaling_group" "example" {
-    launch_configuration = aws_launch_configuration.example.name
+    launch_template {
+      id = aws_launch_template.example.id
+      version = "$Latest"
+    }
+
     vpc_zone_identifier = data.aws_subnets.default.ids
 
     target_group_arns = [aws_lb_target_group.asg.arn]
@@ -41,7 +55,7 @@ resource "aws_autoscaling_group" "example" {
 
 # creates security group
 resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
+    name = "terraform-example-instance-sg"
 
     ingress {
         from_port = var.server_port
