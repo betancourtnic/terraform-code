5fc3f406e5031822a86bf6ad45752e3915f10bdc
nbetancourt <206279116+betancourtnic@users.noreply.github.com>
2025-07-16 15:09:10 -0500

    add ASG target group & health check

--- terraform-up-and-running-code-examples/ec2-instance-example/webserver-cluster/main.tf
+++ terraform-up-and-running-code-examples/ec2-instance-example/webserver-cluster/main.tf
@@ -26,6 +26,9 @@ resource "aws_autoscaling_group" "example" {
     launch_configuration = aws_launch_configuration.example.name
     vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
+
     min_size = 2
     max_size = 10
 
@@ -102,5 +105,24 @@ resource "aws_security_group" "alb" {
         to_port = 0
         protocol = "-1"
         cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+#create target group for ASG
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    
+    #health check to each Instance
+    health_check {
+        path    = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
     }
 }
