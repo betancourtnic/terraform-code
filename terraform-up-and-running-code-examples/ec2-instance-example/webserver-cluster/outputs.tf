34753c05dbdde89815d4278e90956d1f65551a9e
nbetancourt <206279116+betancourtnic@users.noreply.github.com>
2025-07-16 17:47:27 -0500

    removed output for aws instance and replaced with output for alb dns name

--- terraform-up-and-running-code-examples/ec2-instance-example/webserver-cluster/outputs.tf
+++ terraform-up-and-running-code-examples/ec2-instance-example/webserver-cluster/outputs.tf
@@ -1,9 +1,5 @@
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
-}
-
-output "instance_id" {
-    value = aws_instance.example.id
-    description = "The public id running instance"
+#output shows DNS name of the ALB
+output "alb_dns_name" {
+    value = aws_lb.example.dns_name
+    description = "The domain name of the load balancer"
 }
